7c29d07a33a7aeed6560a14e0e57b1d1e5f836c2
edwinchen85 <edwinchen85@gmail.com>
2017-09-18 21:55:04 +0800

    Add animation to blue box

--- main.js
+++ main.js
@@ -9,3 +9,16 @@ anime({
     easing: 'easeInOutSine'
   }
 });
+
+anime({
+  targets: 'div.box.blue',
+  translateY: [
+    { value: 200, duration: 500, delay: 1000 },
+    { value: 0, duration: 800 }
+  ],
+  rotate: {
+    value: '1turn',
+    easing: 'easeInOutSine',
+    delay: 1000
+  }
+});
